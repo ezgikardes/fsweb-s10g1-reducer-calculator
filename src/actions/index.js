5e8e85d89e7ca3338575ca5359d7c88ff6ec1816
ezgikardes <ezgikardes@gmail.com>
2024-01-21 21:17:14 +0100

    proje tamamlandı

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,10 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const SET_TOTAL = "SET_TOTAL";
+export const APPLY_MR = "APPLY_MR"
+export const MEMORY_CLEAR = "MEMORY_CLEAR" 
+export const CLEAR_ALL = "CLEAR_ALL"
 
 export const addOne = () => {
   return ({ type: ADD_ONE });
@@ -10,3 +14,24 @@ export const addOne = () => {
 export const applyNumber = (number) => {
   return ({ type: APPLY_NUMBER, payload: number });
 }
+
+export const changeOperation = (operation) => {
+  return ({ type: CHANGE_OPERATION, payload: operation });
+} //changeOperation isimli bir action yazdık. parametre olarak operation değerini alacak. Bize bir obje döndürecek. Bu objenin type'ı CHANGE_OPERATION, payload'ı da argümandan gelen operation.
+
+
+export const setTotal = () => ({
+    type: SET_TOTAL, 
+}) //SET_TOTAL (bastığımızda mevcut değeri memory'ye kaydedecek M+ butonu) için action creator fonksiyonu yazdık. 
+
+export const applyMr = () => ({
+    type: APPLY_MR,
+}) // APPLY_MR için action creator yazdık. 
+
+export const memoryClear = () => ({
+  type: MEMORY_CLEAR,
+}) // MEMORY_CLEAR için action creator yazdık.
+
+export const clearAll = () => ({
+  type: CLEAR_ALL,
+}) // CLEAR_ALL için action creator yazdık.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, SET_TOTAL, APPLY_MR, MEMORY_CLEAR, CLEAR_ALL } from './../actions';
 
 export const initialState = {
-  total: 100,
-  operation: "*",
-  memory: 100
+  total: 0,
+  operation: "+",
+  memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -38,6 +38,35 @@ const reducer = (state, action) => {
         ...state,
         operation: action.payload
       });
+
+    //bir case daha ekleyelim. (bastığımızda mevcut değeri memory'ye kaydedecek M+ butonu için) SET_TOTAL çalıştırdığında bize memory değerini state.total olarak döndürsün. 
+    case (SET_TOTAL):
+      return ({
+        ...state,
+        memory: state.total,
+      });
+
+    // applyMr için bir case daha ekliyoruz. bastığımızda, mevcut değere, memory değerine setleyip göndersin.
+    case (APPLY_MR): 
+      return ({
+        ...state,
+        total: calculateResult(state.total, state.memory, state.operation),
+      })
+
+      
+    //memoryClear için bir case daha ekliyoruz. bastığımızda memory'i sıfırlayıp göndersin.
+    case (MEMORY_CLEAR):
+      return ({
+        ...state,
+        memory: 0,
+      })
+
+    //clearAll için bir case daha ekliyoruz. bastığımızda state'in total field'ını sıfırlayıp göndersin.
+    case (CLEAR_ALL):
+      return ({
+        ...state,
+        total: 0,
+      })
 
     default:
       return state;
